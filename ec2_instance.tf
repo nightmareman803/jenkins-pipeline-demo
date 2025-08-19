901dab424edcd21bdddab6f5fce45f42e668d186
nightmareman803 <nightmareman803@gmail.com>
2025-08-19 11:45:53 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2" # change to your region
+}
 resource "aws-instance" "myec2-test" {
   ami = "ami-0b016c703b95ecbe4"
   instance_type = "t3.micro"
